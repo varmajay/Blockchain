5b5746103f029667e7a1298e9f473c4c04104d45
Jay varma <jayshankarvarma121@gmail.com>
2022-07-20 14:24:49 +0530

    web3

--- web3/CheckBalance.py
+++ web3/CheckBalance.py
@@ -10,3 +10,6 @@ balance = web3.eth.get_balance("0x5A9cBc9821cF17eC0823593b6a2daD9e72a053BC")
 print(balance)
 web3.fromWei(balance,"ether")
 print(web3.fromWei(balance,"ether"))
+
+transaction = web3.eth.get_transaction('0xd93b2f78aa398ef2fc3f004b6f22ff9f00e78fc6c51cd694ab09b0b457318b93')
+print(transaction)
